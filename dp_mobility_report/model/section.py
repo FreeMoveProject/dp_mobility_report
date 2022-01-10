15610e34ac97e90f57cf33cb94d1de0e181bf1e1
AlexandraKapp <alexandra.kapp2@gmail.com>
2022-01-10 15:32:33 +0100

    make section a dataclass

--- dp_mobility_report/model/section.py
+++ dp_mobility_report/model/section.py
@@ -1,31 +1,30 @@
 from dp_mobility_report import constants as const
+from dataclasses import dataclass
+import numpy as np
+import pandas as pd
+from typing import Union, Tuple
 
-
+@dataclass
 class Section:
     """Class for single report sections. Attribute 'data' contains the content of the report element.
     'privacy_budget' contains information on the used privacy_budget to create this report element.
     'n_outliers' contains information of outliers, if there are potential outliers for this report element - otherwise it defaults to `None`.
     Potential further attributes: `quartiles`, `datetime_precision`.
     """
+    data: Union[pd.Series, np.array, Tuple]
+    privacy_budget: float
+    n_outliers: int = None
+    quartiles: np.array = None
+    datetime_precision: str = None
 
-    def __init__(self, data, privacy_budget, n_outliers=None, **kwargs) -> None:
-
-        self.data = data
-        self.n_outliers = n_outliers
-        self.privacy_budget = privacy_budget
-        if const.QUARTILES in kwargs:
-            self.quartiles = kwargs[const.QUARTILES]
-        if const.DATETIME_PRECISION in kwargs:
-            self.datetime_precision = kwargs[const.DATETIME_PRECISION]
-
-    def __str__(self):
-        print_output = {
-            "data": self.data,
-            "privacy_budget": self.privacy_budget,
-            "n_outlier": self.n_outliers,
-        }
-        if hasattr(self, const.QUARTILES):
-            print_output[const.QUARTILES] = self.quartiles
-        if hasattr(self, const.DATETIME_PRECISION):
-            print_output[const.DATETIME_PRECISION] = self.datetime_precision
-        return print_output.__str__()
+    # def __str__(self):
+    #     print_output = {
+    #         "data": self.data,
+    #         "privacy_budget": self.privacy_budget,
+    #         "n_outlier": self.n_outliers,
+    #     }
+    #     if hasattr(self, const.QUARTILES):
+    #         print_output[const.QUARTILES] = self.quartiles
+    #     if hasattr(self, const.DATETIME_PRECISION):
+    #         print_output[const.DATETIME_PRECISION] = self.datetime_precision
+    #     return print_output.__str__()
