3e5aa5e6614c987ecb44f55214eff5a2ac84f118
AlexandraKapp <alexandra.kapp2@gmail.com>
2022-01-10 15:32:20 +0100

    linting: change to f-string

--- dp_mobility_report/visualization/v_utils.py
+++ dp_mobility_report/visualization/v_utils.py
@@ -7,8 +7,6 @@ def fig_to_html(fig):
     img = io.BytesIO()
     fig.savefig(img, format="png", bbox_inches="tight")
     img.seek(0)
-
-    html = '<div><img src="data:image/png;base64, {}"></div>'.format(
-        base64.b64encode(img.getvalue()).decode("utf-8")
-    )
+    img_string = base64.b64encode(img.getvalue()).decode("utf-8")
+    html = f"<div><img src='data:image/png;base64,{img_string}'></div>"
     return html
