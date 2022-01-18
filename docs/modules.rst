e5749d00ab731fe3434f4cae5146d859c79427f6
AlexandraKapp <alexandra.kapp2@gmail.com>
2022-01-18 11:44:22 +0100

    update doc

---
+++ docs/modules.rst
@@ -0,0 +1,7 @@
+dp_mobility_report
+==================
+
+.. toctree::
+   :maxdepth: 4
+
+   dp_mobility_report
--- dp_mobility_report/md_report.py
+++ dp_mobility_report/md_report.py
@@ -19,7 +19,27 @@ class MobilityDataReport:
     a pandas `DataFrame`. Expected columns: User ID `uid`, Trip ID `tid`, Timestamp `datetime`,
     Latitude and Longitude in CRS EPSG:4326 `lat` and `lng`.
        The report will be generated as an HTML file, using the `.to_html()` method.
-    """
+
+    Args:
+        df: pandas `DataFrame` containing the mobility data. Expected columns: User ID `uid`, Trip ID `tid`, Timestamp `datetime`,
+            Latitude and Longitude in CRS EPSG:4326 `lat` and `lng`.
+        tessellation: Geopandas `GeoDataFrame` containing the tessellation for spatial aggregations. If tessellation is not provided
+            in the expected default CRS EPSG:4326 it will automatically be transformed.
+        privacy_budget: privacy_budget for the differentially private report
+        user_privacy: Whether item-level or user-level privacy is applied. Defaults to True (user-level privacy).
+        max_trips_per_user: maximum number of trips a user shall contribute to the data. Dataset will be sampled accordingly.
+        analysis_selection: Select only needed analyses. A selection reduces computation time and leaves more privacy budget
+            for higher accuracy of other analyses. Options are `overview`, `place_analysis`, `od_analysis`, `user_analysis` and `all`. Defaults to [`all`].
+        disable_progress_bar: Whether progress bars should be shown. Defaults to False.
+        timewindows: List of hours as `int` that define the timewindows for the spatial analysis for single time windows. Defaults to [2, 6, 10, 14, 18, 22].
+        max_travel_time: Upper bound for travel time analysis to cut outliers based on domain knowledge. If None is given, no upper bound is set. Defaults to None.
+        bin_range_travel_time: The range a single histogram bin spans for travel time (e.g., 5 for 5 min bins). If None is given, the histogram bins will be determined automatically. Defaults to None.
+        max_jump_length: Upper bound for jump length analysis to cut outliers based on domain knowledge. If None is given, no upper bound is set. Defaults to None.
+        bin_range_jump_length: The range a single histogram bin spans for jump length (e.g., 1 for 1 km bins). If None is given, the histogram bins will be determined automatically. Defaults to None.
+        max_radius_of_gyration: Upper bound for radius of gyration analysis to cut outliers based on domain knowledge. If None is given, no upper bound is set. Defaults to None.
+        bin_range_radius_of_gyration The range a single histogram bin spans for the radius of gyration (e.g., 1 for 1 km bins). If None is given, the histogram bins will be determined automatically. Defaults to None.
+        evalu (bool, optional): Parameter only needed for development and evaluation purposes. Defaults to False.
+        """
 
     _report: dict = {}
     _html: str = ""
@@ -42,29 +62,6 @@ class MobilityDataReport:
         bin_range_radius_of_gyration: Optional[Union[int, float]] = None,
         evalu: bool = False,
     ) -> None:
-        """Generate a (differentially private) mobility report from a dataset stored as
-        a pandas `DataFrame`.
-
-        Args:
-            df: pandas `DataFrame` containing the mobility data. Expected columns: User ID `uid`, Trip ID `tid`, Timestamp `datetime`,
-                Latitude and Longitude in CRS EPSG:4326 `lat` and `lng`.
-            tessellation: Geopandas `GeoDataFrame` containing the tessellation for spatial aggregations. If tessellation is not provided
-                in the expected default CRS EPSG:4326 it will automatically be transformed.
-            privacy_budget: privacy_budget for the differentially private report
-            user_privacy: Whether item-level or user-level privacy is applied. Defaults to True (user-level privacy).
-            max_trips_per_user: maximum number of trips a user shall contribute to the data. Dataset will be sampled accordingly.
-            analysis_selection: Select only needed analyses. A selection reduces computation time and leaves more privacy budget
-                for higher accuracy of other analyses. Options are `overview`, `place_analysis`, `od_analysis`, `user_analysis` and `all`. Defaults to [`all`].
-            disable_progress_bar: Whether progress bars should be shown. Defaults to False.
-            timewindows: List of hours as `int` that define the timewindows for the spatial analysis for single time windows. Defaults to [2, 6, 10, 14, 18, 22].
-            max_travel_time: Upper bound for travel time analysis to cut outliers based on domain knowledge. If None is given, no upper bound is set. Defaults to None.
-            bin_range_travel_time: The range a single histogram bin spans for travel time (e.g., 5 for 5 min bins). If None is given, the histogram bins will be determined automatically. Defaults to None.
-            max_jump_length: Upper bound for jump length analysis to cut outliers based on domain knowledge. If None is given, no upper bound is set. Defaults to None.
-            bin_range_jump_length: The range a single histogram bin spans for jump length (e.g., 1 for 1 km bins). If None is given, the histogram bins will be determined automatically. Defaults to None.
-            max_radius_of_gyration: Upper bound for radius of gyration analysis to cut outliers based on domain knowledge. If None is given, no upper bound is set. Defaults to None.
-            bin_range_radius_of_gyration The range a single histogram bin spans for the radius of gyration (e.g., 1 for 1 km bins). If None is given, the histogram bins will be determined automatically. Defaults to None.
-            evalu (bool, optional): Parameter only needed for development and evaluation purposes. Defaults to False.
-        """
         _validate_input(
             df,
             tessellation,
