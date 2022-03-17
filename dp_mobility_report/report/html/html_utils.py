55dba19c915c1e7717cdc4e72fbeb9fad3b26907
AlexandraKapp <alexandra.kapp2@gmail.com>
2022-03-17 11:58:30 +0100

    remove margin of error estimate for exponential mechanism

--- dp_mobility_report/report/html/html_utils.py
+++ dp_mobility_report/report/html/html_utils.py
@@ -63,8 +63,8 @@ def render_moe_info(margin_of_error: int) -> str:
     return f"""To provide privacy, quartile values are not necessarily the true values but, e.g., instead of the true maximum value the 
         second or third highest value is displayed.
         This is achieved by the so-called exponential mechanism, where a value is drawn based on probabilites defined by the privacy budget. 
-        Generally, a value closer to the true value has a higher chance of being drawn.
-        The true quartile values lie with a <b>95% chance ± {margin_of_error} records</b> away from the true values."""
+        Generally, a value closer to the true value has a higher chance of being drawn."""
+    #The true quartile values lie with a <b>95% chance within ± {margin_of_error} records</b> away from the true values.""" # TODO: margin_of_error reveals true record count?
 
 
 def fmt(value: Any) -> Any:
