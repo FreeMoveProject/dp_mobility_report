6e5d6d25cf8095231eb51a380629b5ce241e37d9
AlexandraKapp <alexandra.kapp2@gmail.com>
2022-07-22 15:32:08 +0200

    update version

--- setup.py
+++ setup.py
@@ -48,6 +48,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/FreeMoveProject/dp_mobility_report",
-    version="0.0.2",
+    version="0.0.3",
     zip_safe=False,
 )
