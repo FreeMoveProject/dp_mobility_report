e298b536f11bc0c393153ae35bef33056ac3d99a
Saskia <saskia.nunezvonvoigt@tu-berlin.de>
2022-08-13 10:49:20 +0200

    update test md

--- tests/test_md_report.py
+++ tests/test_md_report.py
@@ -280,12 +280,12 @@ def test_to_html_file(test_data, test_tessellation, tmp_path):
     file_name = tmp_path / "html/test_output1.html"
     file_name.parent.mkdir()
     md_report.MobilityDataReport(
-        test_data, test_tessellation, privacy_budget=None, timestamps=True
+        test_data, test_tessellation, privacy_budget=None
     ).to_file(file_name)
     assert file_name.is_file()
 
     file_name = tmp_path / "html/test_output2.html"
     md_report.MobilityDataReport(
-        test_data, test_tessellation, privacy_budget=0.1, timestamps=True
+        test_data, test_tessellation, privacy_budget=0.1
     ).to_file(file_name)
     assert file_name.is_file()
