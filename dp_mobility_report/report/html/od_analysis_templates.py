6ae60b5d1db8e02f6d341a9e159cc7f6ab3393c6
AlexandraKapp <alexandra.kapp2@gmail.com>
2023-03-22 17:26:17 +0100

    bug fix

--- dp_mobility_report/report/html/od_analysis_templates.py
+++ dp_mobility_report/report/html/od_analysis_templates.py
@@ -58,7 +58,9 @@ def render_od_analysis(
             report[const.OD_FLOWS], len(dpmreport.tessellation)
         )
         quartiles = report[const.OD_FLOWS].quartiles.round()
-        args["flows_summary_table"] = render_summary(quartiles.astype(int))
+        if not any(np.isnan(quartiles)):
+            quartiles = quartiles.astype(int)
+        args["flows_summary_table"] = render_summary(quartiles)
         args["flows_cumsum_linechart"] = render_flows_cumsum(
             report[const.OD_FLOWS], diagonal=True
         )
