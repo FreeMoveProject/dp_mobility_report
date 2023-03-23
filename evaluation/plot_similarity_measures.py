a15ea34c6079f77b66f4c3f7ad9ae3b93dd695d6
brunkowacodes <post_fuer_sanny@yahoo.de>
2023-03-23 16:33:06 +0100

    Changed Plotting, so that average values for all selected epsilons and algorithms are shown in one plot for every selected similarity measure

--- evaluation/plot_similarity_measures.py
+++ evaluation/plot_similarity_measures.py
@@ -9,7 +9,7 @@ import numpy as np
 # State which runs you want to plot
 ALGORITHMS = ["DPSTAR", "DP"]
 NUMBER_OF_RUNS = 10
-EPSILON = 4
+EPSILON = [4, 10, 20, 50]
 MAX_TRIPS = "None"
 USER_PRIVACY = False
 MEASURES_TO_PLOT = ["n_records", "n_users", "n_locations", "visits_per_tile", "user_tile_count"]
@@ -20,36 +20,42 @@ if os.path.isfile(evaluation_config.PATH_CSV_FILE):
 else:  # if it doesn't exist exit with error message
     sys.exit("ERROR: No CSV to read")
 
-# filter csv for selected data (except Measures to Plot)
-algorithm_datas = []
-for algorithm in ALGORITHMS:
-    algorithm_datas.append(whole_csv_file_df[(whole_csv_file_df["Algorithm"] == algorithm) & (whole_csv_file_df["Raw/Average"] == "RAW") & (
-                whole_csv_file_df["Epsilon"] == EPSILON) & (whole_csv_file_df["User Privacy"] == USER_PRIVACY) & (
-                                                         whole_csv_file_df["Max Trips"] == MAX_TRIPS)])
+averages_df = pd.DataFrame()  # Dataframe for the average values (for given settings)
+#
+for epsilon in EPSILON:
+    algorithm_datas = []
+    for algorithm in ALGORITHMS:
+        algorithm_data = whole_csv_file_df[(whole_csv_file_df["Algorithm"] == algorithm) & (whole_csv_file_df["Raw/Average"] == "RAW") & (
+                    whole_csv_file_df["Epsilon"] == epsilon) & (whole_csv_file_df["User Privacy"] == USER_PRIVACY) & (
+                                                             whole_csv_file_df["Max Trips"] == MAX_TRIPS)]
+        algorithm_data_mean = pd.DataFrame(algorithm_data.mean(numeric_only=True)).T
+        algorithm_data_mean.insert(0, column="Algorithm", value=algorithm)
+        averages_df = pd.concat([averages_df, pd.DataFrame(algorithm_data_mean)], ignore_index=True)
 
-# filter dataframes for selected similarity measures and plot them
+# filtering the average dataframe for the columns selected to plot
+meta_columns = ["Algorithm", "Epsilon"]
+selected_measures = pd.DataFrame(averages_df, columns=meta_columns + MEASURES_TO_PLOT).reset_index(drop=True)
+
+# Filters for every selected measure and plots the averages in comparison
 for measure in MEASURES_TO_PLOT:
-    algorithms_selected_columns = []  # Array for datasets for each algorithm
-    for algorithm_data in algorithm_datas:
-        algorithms_selected_columns.append(pd.DataFrame(algorithm_data, columns=[measure]).reset_index(drop=True))
-
-    # plot them
+    selected_measure = pd.DataFrame(selected_measures, columns=meta_columns + [measure]).reset_index(drop=True)
     fig = plt.figure()
     ax = plt.gca()
-    for idx, algorithm_selected_columns in enumerate(algorithms_selected_columns):
-        color = next(ax._get_lines.prop_cycler)['color']
-        plt.plot(algorithm_selected_columns, color=color, marker="X", markersize="10", linestyle="", label=ALGORITHMS[idx] + " Raw")
-        plt.axhline(np.nanmean(algorithm_selected_columns), color=color, linestyle="--", dashes=(5, 7), label=ALGORITHMS[idx] + " Average")
-
+    for algorithm in ALGORITHMS:
+        algorithm_averages = selected_measure[selected_measure["Algorithm"] == algorithm].reset_index(drop=True)
+        algorithm_averages.plot(x=0, y=measure, marker="X", markersize="10", linestyle="", label=algorithm, ax=ax)
+        ax.set_xticks(range(len(EPSILON)))
+        ax.set_xticklabels(EPSILON)
+        #algorithm_averages.plot.bar(x="Epsilon", y=measure, label=algorithm, ax=ax)
     plt.title(measure)
-    plt.xlabel("Run")
+    plt.xlabel("Epsilon")
     plt.ylabel(measure + " similarity measure")
     plt.legend()
-    # generate filename
+    # Save the plots as png
     algorithm_names = ""
     for algorithm in ALGORITHMS:
         algorithm_names += (algorithm + "_")
-    filename = algorithm_names + "nr_runs_" + str(NUMBER_OF_RUNS) + "_epsilon_" + str(EPSILON) + "_maxtrips_" + str(
+    filename = algorithm_names + "_epsilon_" + str(EPSILON) + "_maxtrips_" + str(
         MAX_TRIPS) + "_userprivacy_" + str(USER_PRIVACY) + "_measure_" + measure
     plt.savefig(evaluation_config.PATH_PLOTS + "/" + filename + ".png")
 
