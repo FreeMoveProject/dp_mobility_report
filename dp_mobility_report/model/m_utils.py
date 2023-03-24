c39d40cc1c47c3f8309fe1b2ebdc6b4fc49eb057
AlexandraKapp <alexandra.kapp2@gmail.com>
2023-03-24 15:41:32 +0100

    fix shape mismatch bug
    if there are only values about hist_max, not the correct amount of bins are created for bin_range=1

--- dp_mobility_report/model/m_utils.py
+++ dp_mobility_report/model/m_utils.py
@@ -68,13 +68,8 @@ def hist_section(
     # min value of histogram: either given as input or determined by dp min
     hist_min = hist_min if (hist_min is not None) else _round_down(quartiles["min"], 3)
 
-    # if all values above defined max, create one histogram bin greater hist_max
-    if hist_min > hist_max:
-        bins = np.array([hist_max, np.inf])
-        counts = np.array([len(series)])
-
     # if there are max. 10 integers, use value counts of single integers instead of bin ranges for histogram
-    elif (bin_type is int) and (
+    if (bin_type is int) and (
         (bin_range == 1) or ((bin_range is None) and (hist_max - hist_min < 10))
     ):
         bins = np.array(range(int(quartiles["min"]), int(quartiles["max"]) + 1))
@@ -90,6 +85,12 @@ def hist_section(
                 len(bins) - 1 :
             ].sum()  # sum up to single > max count
             counts = counts[: len(bins)]  # remove long tail
+
+
+    # if all values above defined max, create one histogram bin greater hist_max
+    elif hist_min > hist_max:
+        bins = np.array([hist_max, np.inf])
+        counts = np.array([len(series)])
 
     # else use ranges for bins to create histogram
     else:
